bb09db5b25ebd2edf87ce505dcebc7321d60a07b
User <akramovabdullo2002@gmail.com>
2022-12-20 18:01:11 +0500

    add update

--- update-category.php
+++ update-category.php
@@ -1,6 +1,8 @@
 <?php
 if ($_POST) {
     require "categories-model.php";
-//    $category = getCategory($_POST['id']);
+    update($_POST['id'],$_POST['name']);
+    header('Location: products.php');
+    exit();
 
 }
